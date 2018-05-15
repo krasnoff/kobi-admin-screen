e3797b4f7f7af92fe5c8bb297136ea0628a95a0a
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2018-05-15 19:47:12 +0300

    continue login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { GlobalDataService } from './global-data.service';
 import { LanguagesAware } from './languages-aware.decorator';
 import { Languages } from './languages.enum';
 
+import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,10 +24,27 @@ export class AppComponent {
 
   public closeResult: string;
 
-  constructor(private _httpService:AppService, private modalService: NgbModal, private translate: TranslateService, public gd: GlobalDataService) {
+  constructor(private _httpService:AppService, private modalService: NgbModal, private translate: TranslateService, public gd: GlobalDataService, router:Router) {
     if (this.translate.currentLang == undefined)
       this.translate.currentLang = Languages.English;
     translate.setDefaultLang(this.translate.currentLang);
+
+    router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        if (event.url.indexOf("/login") > -1) {
+          document.getElementById("topNav").style.display = "none";
+          document.getElementById("sidebar").style.display = "none";
+        }
+        else {
+          document.getElementById("topNav").style.display = "flex";
+          document.getElementById("sidebar").style.display = "block";
+        }
+      }
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
+    });
   }
 
   ngOnInit() {
